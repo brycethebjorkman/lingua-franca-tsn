6fa1bb70c4261ae818d400919e81dfbc25e9a44f
brycethebjorkman <46073756+brycethebjorkman@users.noreply.github.com>
2023-11-12 20:55:53 -0600

    Update the basic_federation run script to use the executable OMNeT++ generates

--- lingua-franca-tsn/simulations/basic_federation/run.sh
+++ lingua-franca-tsn/simulations/basic_federation/run.sh
@@ -1,5 +1,33 @@
 # start simulation
-inet -u Cmdenv -f omnetpp.ini -n .
+
+# # some variation of this should be workable as an alternative to calling the executable OMNeT++ generates in the src directory
+# inet -u Cmdenv \
+#        -f $LINGUA_FRANCA_TSN_ROOT/lingua-franca-tsn/simulations/basic_federation/omnetpp.ini \
+#        -n $LINGUA_FRANCA_TSN_ROOT/lingua-franca-tsn/simulations \
+#        &
+
+$LINGUA_FRANCA_TSN_ROOT/lingua-franca-tsn/src/lingua-franca-tsn -m \
+    -n $LINGUA_FRANCA_TSN_ROOT/lingua-franca-tsn/simulations \
+    -n $LINGUA_FRANCA_TSN_ROOT/lingua-franca-tsn/src \
+    -n $INET_ROOT/examples \
+    -n $INET_ROOT/showcases \
+    -n $INET_ROOT/src \
+    -n $INET_ROOT/tests/validation \
+    -n $INET_ROOT/tests/networks \
+    -n $INET_ROOT/tutorials \
+    -x inet.common.selfdoc \
+    -x inet.linklayer.configurator.gatescheduling.z3 \
+    -x inet.showcases.visualizer.osg \
+    -x inet.examples.emulation \
+    -x inet.transportlayer.tcp_lwip \
+    -x inet.showcases.emulation \
+    -x inet.applications.voipstream \
+    -x inet.visualizer.osg \
+    -x inet.examples.voipstream \
+    --image-path=$INET_ROOT/images \
+    -l $INET_ROOT/src/INET \
+    $LINGUA_FRANCA_TSN_ROOT/lingua-franca-tsn/simulations/basic_federation/omnetpp.ini \
+    &
 
 # kill child processes
 trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
