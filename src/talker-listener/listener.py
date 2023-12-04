f94b29dd63cffa99407874926218ecb8f5c994fa
brycethebjorkman <46073756+brycethebjorkman@users.noreply.github.com>
2023-12-04 08:35:29 -0600

    Update talker-listener scripts to accept host and port arguments

--- src/talker-listener/listener.py
+++ src/talker-listener/listener.py
@@ -1,22 +1,36 @@
 import socket
+from argparse import ArgumentParser
 
-HOST = '192.168.3.20'
-PORT = 4004
+def init_argparse() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("--host")
+    parser.add_argument("-p", "--port")
+    return parser
 
 def log(*x):
     print('\033[94m', x, '\033[0m')
 
-log('listener starting')
+def main() -> None:
+    log('listener starting')
+    HOST = '192.168.3.20'
+    PORT = 4004
+    parser = init_argparse()
+    args = parser.parse_args()
+    if args.host:
+        HOST = args.host
+    if args.host:
+        PORT = int(args.port)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind((HOST, PORT))
+        log('listener listening for packets on {0}:{1}'.format(HOST, PORT))
+        i = 0
+        while True:
+            i = i + 1
+            data, addr = s.recvfrom(1024)
+            if not data:
+                break
+            log('listener received packet {0} from {1} with data {2}'.format(i, addr, data))
+    log('listener exiting')
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    s.bind((HOST, PORT))
-    log('listener listening for packets on {0}:{1}'.format(HOST, PORT))
-    i = 0
-    while True:
-        i = i + 1
-        data, addr = s.recvfrom(1024)
-        if not data:
-            break
-        log('listener received packet {0} from {1} with data {2}'.format(i, addr, data))
-
-log('listener exiting')
+if __name__ == "__main__":
+    main()
--- src/talker-listener/talker.py
+++ src/talker-listener/talker.py
@@ -1,22 +1,37 @@
 import socket
 import time
+from argparse import ArgumentParser
 
-HOST = '192.168.2.99'
-PORT = 4004
+def init_argparse() -> ArgumentParser:
+    parser = ArgumentParser()
+    parser.add_argument("--host")
+    parser.add_argument("-p", "--port")
+    return parser
 
 def log(*x):
     print('\033[92m', x, '\033[0m')
 
-log('talker starting')
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    log('talker attempting to send packets to {0}:{1}'.format(HOST, PORT))
-    i = 0
-    while True:
-        i = i + 1
-        log('talker sending data {0}'.format(i), (HOST, PORT))
-        bytecount = s.sendto('Data {0}'.format(i).encode(), (HOST, PORT))
-        log('talker sent {0} bytes'.format(bytecount))
-        time.sleep(1)
+def main() -> None:
+    log('talker starting')
+    HOST = '192.168.2.99'
+    PORT = 4004
+    parser = init_argparse()
+    args = parser.parse_args()
+    if args.host:
+        HOST = args.host
+    if args.port:
+        PORT = int(args.port)
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        log('talker attempting to send packets to {0}:{1}'.format(HOST, PORT))
+        i = 0
+        while True:
+            i = i + 1
+            log('talker sending data {0}'.format(i), (HOST, PORT))
+            bytecount = s.sendto('Data {0}'.format(i).encode(), (HOST, PORT))
+            log('talker sent {0} bytes'.format(bytecount))
+            time.sleep(1)
+    log('talker exiting')
 
-log('talker exiting')
+if __name__ == "__main__":
+    main()
