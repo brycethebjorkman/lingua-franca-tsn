0d78d7679bdf1c3d08415e9cdae792fba497ba91
brycethebjorkman <46073756+brycethebjorkman@users.noreply.github.com>
2023-12-05 19:16:22 -0600

    Update simulation build scripts to create src directory if needed

--- tsn-sim/simulations/basic_federation/build.sh
+++ tsn-sim/simulations/basic_federation/build.sh
@@ -1,10 +1,16 @@
 # make tsn-sim
 
-cd $LINGUA_FRANCA_TSN_ROOT/tsn-sim/src
+src_dir="$LINGUA_FRANCA_TSN_ROOT/tsn-sim/src"
+
+if [ ! -d "$src_dir" ]; then
+    mkdir "$src_dir"
+fi
+
+cd "$src_dir"
 opp_makemake -f --deep -KINET4_5_PROJ=/root/inet4.5 -DINET_IMPORT -L$\(INET4_5_PROJ\)/src -lINET$\(D\)
 make MODE=release -j20 all
 
 # make Lingua Franca reactor
-cd "$LINGUA_FRANCA_TSN_ROOT/src/basic-federation"
+cd "$src_dir/basic-federation"
 stdbuf -i 0 -o 0 -e 0 \
 lfc BasicFederation.lf
--- tsn-sim/simulations/five_host/build.sh
+++ tsn-sim/simulations/five_host/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # make tsn-sim
 
-cd $LINGUA_FRANCA_TSN_ROOT/tsn-sim/src
+src_dir="$LINGUA_FRANCA_TSN_ROOT/tsn-sim/src"
+
+if [ ! -d "$src_dir" ]; then
+    mkdir "$src_dir"
+fi
+
+cd "$src_dir"
 opp_makemake -f --deep -KINET4_5_PROJ=/root/inet4.5 -DINET_IMPORT -L$\(INET4_5_PROJ\)/src -lINET$\(D\)
 make MODE=release -j20 all
